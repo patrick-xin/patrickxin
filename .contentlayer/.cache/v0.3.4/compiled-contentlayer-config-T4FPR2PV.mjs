72446197bba1178da114c12a1097b7dd05e6d1d9
Patrick Xin <patrick.xin.dev@gmail.com>
2024-07-08 23:36:24 +0800

    change package version

--- .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-T4FPR2PV.mjs
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-T4FPR2PV.mjs
@@ -1,4 +1,4 @@
-import type { ComputedFields } from "contentlayer/source-files";
+// contentlayer.config.ts
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import readingTime from "reading-time";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
@@ -8,20 +8,18 @@ import remarkExternalLinks from "remark-external-links";
 import { visit } from "unist-util-visit";
 import rehypePrism from "rehype-prism-plus";
 import remarkGfm from "remark-gfm";
-
-const computedFields: ComputedFields = {
+var computedFields = {
   readingTime: { type: "json", resolve: (doc) => readingTime(doc.body.raw) },
   slug: {
     type: "string",
-    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
   },
   url: {
     type: "string",
-    resolve: (post) => `/post/${post._raw.flattenedPath}`,
-  },
+    resolve: (post) => `/post/${post._raw.flattenedPath}`
+  }
 };
-
-const Post = defineDocumentType(() => ({
+var Post = defineDocumentType(() => ({
   name: "Post",
   filePathPattern: "**/*.mdx",
   contentType: "mdx",
@@ -35,12 +33,11 @@ const Post = defineDocumentType(() => ({
     toc: { type: "boolean", required: true },
     thumbnail: { type: "json", required: false },
     category: { type: "enum", required: true, options: ["web", "ai"] },
-    tags: { type: "list", of: { type: "string" }, required: true },
+    tags: { type: "list", of: { type: "string" }, required: true }
   },
-  computedFields,
+  computedFields
 }));
-
-const contentLayerConfig = makeSource({
+var contentLayerConfig = makeSource({
   contentDirPath: "content/posts",
   documentTypes: [Post],
   mdx: {
@@ -50,9 +47,8 @@ const contentLayerConfig = makeSource({
         visit(tree, (node) => {
           if (node?.type === "element" && node?.tagName === "pre") {
             const [codeEl] = node.children;
-
-            if (codeEl.tagName !== "code") return;
-
+            if (codeEl.tagName !== "code")
+              return;
             node.raw = codeEl.children?.[0].value;
           }
         });
@@ -72,11 +68,14 @@ const contentLayerConfig = makeSource({
       [
         rehypeAutolinkHeadings,
         {
-          behavior: "append",
-        },
-      ],
-    ],
-  },
+          behavior: "append"
+        }
+      ]
+    ]
+  }
 });
-
-export default contentLayerConfig;
+var contentlayer_config_default = contentLayerConfig;
+export {
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-T4FPR2PV.mjs.map
